8b5819b9df625e8374ea958b007d0b712c917382
ThienKim52 <kim.thanthien04@hcmut.edu.vn>
2026-07-18 10:52:08 +0700

    commit logger

---
+++ bookmark-service/pkg/logger/level.go
@@ -0,0 +1,12 @@
+package logger
+
+import "github.com/rs/zerolog"
+
+func SetLogLevel(levelStr string) { 
+	level := zerolog.NoLevel
+	level, err := zerolog.ParseLevel(levelStr)
+	if err != nil {
+		level = zerolog.NoLevel
+	}
+	zerolog.SetGlobalLevel(level)
+}
